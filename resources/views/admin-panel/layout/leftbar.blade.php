61e5797eb02e8c5fc636ab584a77bea71272a03b
rameez-iqbal <rame14491@gmail.com>
2024-10-10 08:16:18 +0500

    css added

--- resources/views/admin-panel/layout/leftbar.blade.php
+++ resources/views/admin-panel/layout/leftbar.blade.php
@@ -3,7 +3,7 @@
 		<li class="{{ request()->routeIs('dashboard') ? 'active' : '' }}"><a href="{{route('pages')}}"><i class="la la-home"></i> <span> Dashboard </span> </a></li>
 		<li class="{{ request()->routeIs('pages') || request()->routeIs('page.create') ? 'active' : '' }}">
 			<a href="#">
-				<i class="zmdi zmdi-file"></i> <span>Pages </span> <i class="la la-angle-right down-arrow"></i>
+				<i class="fa fa-pagelines"></i> <span>Pages </span> <i class="la la-angle-right down-arrow"></i>
 			</a>
 			<ul class="sidebar-submenu">
 				<li class="{{ request()->routeIs('pages') || request()->routeIs('page.create')  ? 'active' : '' }}" ><a href="{{route('pages')}}"><i class="la la-minus"></i> <span> Sections</span> </a></li>
@@ -19,7 +19,7 @@
 		</li>
 		<li class="{{ request()->routeIs('library.types') || request()->routeIs('library.create') ? 'active' : '' }}">
 			<a href="#">
-				<i class=" zmdi zmdi-book-image"></i> <span>Library </span> <i class="la la-angle-right down-arrow"></i>
+				<i class="fa fa-book"></i> <span>Library </span> <i class="la la-angle-right down-arrow"></i>
 			</a>
 			<ul class="sidebar-submenu">
 				<li class="{{ request()->routeIs('library.types') || request()->routeIs('library.create')  ? 'active' : '' }}" ><a href="{{route('library.types')}}"><i class="la la-minus"></i> <span> Create Library Type</span> </a></li>
@@ -38,17 +38,17 @@
 		</li> --}}
 		<li class="{{ request()->routeIs('our.teams') || request()->routeIs('our.teams.create') ? 'active' : '' }}">
 			<a href="{{route('our.teams')}}">
-				<i class="la la-user-plus"></i> <span>Our Team </span>
+				<i class="fa fa-users"></i> <span>Our Team </span>
 			</a>
 		</li>
 		<li class="{{ request()->routeIs('project.index') ? 'active' : '' }}">
 			<a href="{{route('project.index')}}">
-				<i class="la lacomment"></i> <span>Our Projects </span>
+				<i class="fa fa-clone"></i> <span>Our Projects </span>
 			</a>
 		</li>
 		<li class="{{ request()->routeIs('settings') ? 'active' : '' }}">
 			<a href="#">
-				<i class="zmdi zmdi-settings-square"></i> <span>Settings </span> <i class="la la-angle-right down-arrow"></i>
+				<i class="fa fa-cogs"></i> <span>Settings </span> <i class="la la-angle-right down-arrow"></i>
 			</a>
 			<ul class="sidebar-submenu">
 				<li class="{{ request()->routeIs('settings')  ? 'active' : '' }}" ><a href="{{route('settings')}}"><i class="la la-minus"></i> <span> Setting</span> </a></li>
